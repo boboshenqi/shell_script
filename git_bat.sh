5c99fe82e3e7a14f7def8bfd3743882cef6c6425
yanboh <yanboh@tujia.com>
2018-12-20 11:33:58 +0800

    :art: first commit

---
+++ git_bat.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+parameter="$1"
+if [ -z "$parameter" ]
+then echo 'commit message is null,please check it and try again'
+else 
+	git add .
+	msg=":art: ${parameter}"
+	git commit -m $msg
+	echo "----------------------"
+	git push
+fi
