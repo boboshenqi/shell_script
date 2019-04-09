d5d891d2b26cee3c5378bea4f45176ce9d278cf3
yanboh <yanboh@tujia.com>
2019-04-09 16:56:57 +0800

    :art: 增加输出

--- git_bat.sh
+++ git_bat.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git add . && echo "mgit add success----------" && git commit -m "$1" &&echo "mgit commit success-----" && git pull && echo "mgit pull success------"&& git push
+git add . && echo "mgit add success----------" && git commit -m "$1" &&echo "mgit commit success-----" && git pull && echo "mgit pull success------"&& git push && echo "mgit push success-----"
