cab3e02061ff12cf060c86209d5f2000ebf91fa0
yanboh <yanboh@tujia.com>
2019-04-09 16:53:23 +0800

    :art: 优化脚本

--- git_bat.sh
+++ git_bat.sh
@@ -1,20 +1,4 @@
 #!/bin/sh
 
 #check git workspace
-if git add . :
-then
-	echo "mgit:git add faild! check your workspace"
-	return
-fi
-
-#commit pull and push
-if [ -z "$1" ]
-then echo 'commit message is null,please check it and try again'
-else 
-	msg=" $1"
-	git commit -m "$msg"
-	echo "mgit:commit done----------------------"
-	git pull
-	echo "mgit:pull done------------------------"
-	git push
-fi
+git add . && git commit -m "$1" && git pull && git push
