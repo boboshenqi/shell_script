d2b534159db4fb7d06ee973c0d02f4a87b223920
yanboh <yanboh@tujia.com>
2019-04-10 10:38:04 +0800

    :art: shellcheck and find bugs

--- git_commit_push.sh
+++ git_commit_push.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-git add . && echo "mgit add success----------" && git commit -m "$1" &&echo "mgit commit success-----" && git pull && echo "mgit pull success------"&& git push && echo "mgit push success-----"
+git add . && 
+echo "mgit add success-----------" && 
+git commit -m "$1" &&
+echo "mgit commit success--------" &&
+git pull && 
+echo "mgit pull success----------" &&
+git push &&
+echo "mgit push success----------"
--- git_create_branch.sh
+++ git_create_branch.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #create local branch and origin branch
 if [ -z "$1" ]
-	then echo "需要创建的分之不应为空"
+	then echo "需要创建的branch不应为空"
 else
-	git checkout master && git checkout -b $1 && git push origin $1 && git branch --set-upstream-to=origin/$1 $1 && git branch -a || grep $1
+	git checkout master && 
+	git checkout -b "$1" && 
+	git push origin "$1" && 
+	git branch --set-upstream-to=origin/"$1" "$1" && 
+	git branch -a | grep "$1"
 fi
-
