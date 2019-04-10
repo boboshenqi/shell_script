d24e5be4e9fcfa36957177b3f56c25d523b17488
yanboh <yanboh@tujia.com>
2019-04-10 10:12:51 +0800

    :art: add git create new branch

---
+++ git_create_branch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#create local branch and origin branch
+if [ -z "$1" ]
+	then echo "需要创建的分之不应为空"
+else
+	git checkout master && git checkout -b $1 && git push origin $1 && git branch --set-upstream-to=origin/$1 $1 && git branch -a || grep $1
+fi
+
