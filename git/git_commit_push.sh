cd8cde4dcb99948916593c442c999171d42680fd
yanboh <yanboh@tujia.com>
2019-04-10 11:24:32 +0800

    test

--- git/git_commit_push.sh
+++ git/git_commit_push.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+while getopts 'n:' op
+do
+  case $op in
+    n) nopull="yes"
+  esac
+done
+
 git add . && 
 echo "mgit add success-----------" && 
 git commit -m "$1" &&
 echo "mgit commit success--------" &&
-git pull && 
-echo "mgit pull success----------" &&
+if [ -z "$nopull" ]
+then git pull 
+echo "mgit pull success----------" 
+fi && 
 git push &&
 echo "mgit push success----------"
